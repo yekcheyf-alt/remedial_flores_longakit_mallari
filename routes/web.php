9c8de458903c2b66cc41b43d88b7b8d2f98ee50e
marie <mimi3ylv@gmail.com>
2026-05-29 10:38:55 -0700

    check routes

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,12 @@ Route::middleware('auth')->group(function () {
 
     // Employee routes
     Route::get('employee', [\App\Http\Controllers\employeecontroller::class, 'index'])->name('employee.index');
+    Route::get('employee/create', [\App\Http\Controllers\employeecontroller::class, 'create'])->name('employee.create');
+    Route::post('employee', [\App\Http\Controllers\employeecontroller::class, 'store'])->name('employee.store');
+    Route::get('employee/{id}/edit', [\App\Http\Controllers\employeecontroller::class, 'edit'])->name('employee.edit');
+    Route::put('employee/{id}', [\App\Http\Controllers\employeecontroller::class, 'update'])->name('employee.update');
+    Route::delete('employee/{id}', [\App\Http\Controllers\employeecontroller::class, 'destroy'])->name('employee.destroy'); 
+    
 
     
 
